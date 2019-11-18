67cb38f542fb72d3cffd5fa52e305a36d020f16d
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-11-18 16:22:49 +0100

    corrected migration

--- db/schema.rb
+++ db/schema.rb
@@ -51,13 +51,14 @@ ActiveRecord::Schema.define(version: 2019_11_18_145257) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "rentals", "statues"
