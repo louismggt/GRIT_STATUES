97a89019fb3e6c883c027bae228ec3fda7c38f6d
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-11-18 15:59:36 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  resources :rentals, except: [:new, :create]
+  resources :statues do
+    resources :reviews
+    resources :rentals, only: [:new, :create]
+  end
 end
