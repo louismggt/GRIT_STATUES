b24041f2ae72cb6dcdc45089f8ec6bade4c742c7
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-11-19 11:01:24 +0100

    cleaning branches

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -26,6 +26,13 @@ class RentalsController < ApplicationController
     end
   end
 
+  def destroy
+    @rental = Rental.find(params[:id])
+    @rental.destroy
+
+    redirect_to user_path
+  end
+
   private
 
   def rentals_params
--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,7 +1,23 @@
 class ReviewController < ApplicationController
 
   def new
+    @statue = Statue.find(params[:statue_id])
     @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @statue = Statue.find(params[:statue_id])
+  end
+
+  def index
+    @reviews = Review.all(params[:statue_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :stars)
   end
 
 
