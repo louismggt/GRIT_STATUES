e1501d2b50777f92fddd508d6944515f0f5f4b6b
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-11-19 14:22:07 +0100

    statues OK

--- app/controllers/statues_controller.rb
+++ app/controllers/statues_controller.rb
@@ -13,12 +13,37 @@ class StatuesController < ApplicationController
 
   def create
     @statue = Statue.new(statues_params)
+    if @statue.save
+      redirect_to statue_path(@statue)
+    else
+      render :new
+    end
+  end
 
+  def edit
+    @statue = Statue.find(params[:id])
+  end
+
+  def update
+    @statue = Statue.find(params[:id])
+    @statue.update(statues_params)
+
+    if @statue.save
+      redirect_to statue_path(@statue)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @statue = Statue.find(params[:id])
+    @statue.destroy
   end
 
   private
 
   def statues_params
-    params.require(:statue).permit(:name, :description, :length, :width, :height, :weight, :price, :category, :photo)
+    params.require(:statue).permit(:name, :description, :length, :width, :height,
+                                   :weight, :material, :price, :category, :photo)
   end
 end
