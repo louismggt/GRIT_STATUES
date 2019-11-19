ed338a970c16ed7fd09ccff8fba2de991224b576
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-11-19 18:14:40 +0100

    map available for show

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,38 +2,39 @@ import mapboxgl from 'mapbox-gl';
 
 const mapElement = document.getElementById('map');
 
-const buildMap = () => {
+const buildMap = (marker) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [marker.lng, marker.lat],
+    zoom: 9
   });
 };
 
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
+const addMarkersToMap = (map, marker) => {
     new mapboxgl.Marker()
+
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
-  });
+
 };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, minZoom: 15 });
 };
 
 const initMapbox = () => {
   if (mapElement) {
-    const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
+    const map = buildMap(markers);
+
     addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
+    // fitMapToMarkers(map, markers);
   }
 };
 
 export { initMapbox };
-export { buildMap };
-export { addMarkersToMap };
-export { fitMapToMarkers };
+
