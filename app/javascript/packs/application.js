7ecd527bbee6d8473a1b3e40d0de0f0c7d928451
Julie Paturel <patujulie@gmail.com>
2019-11-21 15:42:44 +0100

    add ratings stars

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,12 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
+import { initStarRating } from '../plugins/init_star_rating';
+
+
 import { initMapbox } from '../plugins/init_mapbox';
 
+initStarRating();
+
 initMapbox();
+
