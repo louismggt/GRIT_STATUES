18ad8e37952e46f651b4a653548dc9058da17cd1
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-11-21 16:27:10 +0100

    javascript conflict resolved

--- app/javascript/plugins/price_calculator.js
+++ app/javascript/plugins/price_calculator.js
@@ -2,20 +2,23 @@ const priceUpdate = document.getElementById("range_start");
 const element = document.getElementById("result")
 console.log("toto")
 
-priceUpdate.addEventListener("change", event => {
-    const startDate = document.getElementById("range_start").value;
-    const endDate = document.getElementById("range_end").value;
-    const pricePerDay = document.getElementById("price").dataset.price;
-    console.log("startDate:", startDate);
-    console.log("endDate:", endDate);
-    console.log("pricePerDay:", pricePerDay);
 
-    const price = ((Date.parse(endDate) - Date.parse(startDate)) * pricePerDay) / 86400000;
-    element.innerHTML = price;
+if (priceUpdate) {
+  priceUpdate.addEventListener("change", event => {
+      const startDate = document.getElementById("range_start").value;
+      const endDate = document.getElementById("range_end").value;
+      const pricePerDay = document.getElementById("price").dataset.price;
+      console.log("startDate:", startDate);
+      console.log("endDate:", endDate);
+      console.log("pricePerDay:", pricePerDay);
 
-    if (!isNaN(price)) {
-      const totalPrice = document.getElementById('total-price')
-      totalPrice.classList.remove("d-none");
+      const price = ((Date.parse(endDate) - Date.parse(startDate)) * pricePerDay) / 86400000;
+      element.innerHTML = price;
+
+      if (!isNaN(price)) {
+        const totalPrice = document.getElementById('total-price')
+        totalPrice.classList.remove("d-none");
+      }
     }
-  }
-)
+  )
+}
